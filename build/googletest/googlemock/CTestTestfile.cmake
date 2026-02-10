a040c1f1c0939d772ad804c35ba3be793a567993
Quaant <egor.zhulin333@gmail.com>
2026-02-10 20:54:13 +0300

    configure and cmake

---
+++ build/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/quaant/source/repos/maps/googletest/googlemock
+# Build directory: /home/quaant/source/repos/maps/build/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
